b72052ef6ff03c32346a2d55a3301cd0071d9ef4
pStrachota <p.strachota52@gmail.com>
2022-10-25 13:26:41 +0200

    Initial commit

---
+++ src/main/java/pl/lodz/p/pas/HelloApplication.java
@@ -0,0 +1,9 @@
+package pl.lodz.p.pas;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
